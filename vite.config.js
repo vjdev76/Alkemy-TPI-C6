bdec50db1e9f53741932eeec8a3a2eea79fdde46
Dardo Leguizamon <dardoleguizamon@gmail.com>
2023-12-26 20:59:59 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// actualizo vite config
 export default defineConfig({
+  base: '/Alkemy-TPI/', 
   plugins: [react()],
 })
