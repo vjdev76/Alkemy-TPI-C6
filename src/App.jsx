1b00f6aff39e85d7a8ebe3aa185d8bedcb275e24
Dardo <dardoleguizamon@gmail.com>
2023-12-27 12:46:56 -0300

    modifico comportamiento boton ayuda

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,57 @@
-import React, { useState } from 'react';
-import ProductList from './components/ProductList';
-import ShoppingCart from './components/ShoppingCart';
-import HelpModal from './components/HelpModal';
-import './App.css';
+import React, { useState } from "react";
+import ProductList from "./components/ProductList";
+import ShoppingCart from "./components/ShoppingCart";
+import HelpModal from "./components/HelpModal";
+import "./App.css";
 
 function App({ products }) {
   const [cart, setCart] = useState([]);
   const [showHelpModal, setShowHelpModal] = useState(false);
 
   const addToCart = (product) => {
-    const existingItem = cart.find(item => item.product.id === product.id);
+    const existingItem = cart.find((item) => item.product.id === product.id);
 
     if (existingItem) {
-      setCart(cart.map(item => item.product.id === product.id
-        ? { ...item, quantity: item.quantity + 1 }
-        : item
-      ));
+      setCart(
+        cart.map((item) =>
+          item.product.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        )
+      );
     } else {
       setCart([...cart, { product, quantity: 1 }]);
     }
   };
 
   const adjustQuantity = (product, type) => {
-    setCart(prevCart => {
-      return prevCart.map(item => item.product.id === product.id
-        ? { ...item, quantity: type === 'increment' ? item.quantity + 1 : Math.max(0, item.quantity - 1) }
-        : item
-      ).filter(item => item.quantity > 0); // Filtrar elementos con cantidad mayor que 0
+    setCart((prevCart) => {
+      return prevCart
+        .map((item) =>
+          item.product.id === product.id
+            ? {
+                ...item,
+                quantity:
+                  type === "increment"
+                    ? item.quantity + 1
+                    : Math.max(0, item.quantity - 1),
+              }
+            : item
+        )
+        .filter((item) => item.quantity > 0); // Filtrar elementos con cantidad mayor que 0
     });
   };
 
   const calculateTotal = () => {
-    return cart.reduce((total, item) => total + item.quantity * item.product.price, 0);
+    return cart.reduce(
+      (total, item) => total + item.quantity * item.product.price,
+      0
+    );
   };
 
   const checkout = () => {
     // Aca iria la logica del checkout
-    alert('Compra confirmada. Total: $' + calculateTotal());
+    alert("Compra confirmada. Total: $" + calculateTotal());
   };
 
   const toggleHelpModal = () => {
@@ -46,9 +61,16 @@ function App({ products }) {
   return (
     <div>
       <ProductList products={products} addToCart={addToCart} />
-      <ShoppingCart cart={cart} adjustQuantity={adjustQuantity} calculateTotal={calculateTotal} checkout={checkout} />
+      <ShoppingCart
+        cart={cart}
+        adjustQuantity={adjustQuantity}
+        calculateTotal={calculateTotal}
+        checkout={checkout}
+      />
       {showHelpModal && <HelpModal onClose={toggleHelpModal} />}
-      <button onClick={toggleHelpModal}>Mostrar Ayuda</button>
+      {!showHelpModal && (
+        <button onClick={toggleHelpModal}>Mostrar Ayuda</button>
+      )}
     </div>
   );
 }
